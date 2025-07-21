3d5562924673a49184e7d0edb78ce851fb8a4ac7
madison mirelez <madis123879@gmail.com>
2025-07-20 19:59:06 -0500

    feat: homepage is rendered with js and css both have own module now

---
+++ src/components/homepage.js
@@ -0,0 +1,51 @@
+import homepageImg from '../images/homepage-food.png';
+import './homepage.css'
+
+export default function loadHome(){
+    const content = document.querySelector('#content');
+
+    const textContainer = document.createElement('div');
+    textContainer.id = 'text-content';
+
+    const restaurantName = document.createElement('h3');
+    restaurantName.id = 'name';
+    restaurantName.textContent = 'Yummy';
+
+
+    const span = document.createElement('span');
+    span.textContent = ' Kitchen';
+    span.classList.add('logo-color');
+
+    restaurantName.appendChild(span);
+    
+
+    const tagLine = document.createElement('h1');
+    tagLine.id = 'tag';
+    tagLine.textContent = 'Delicious Dishes for Food Lovers';
+
+    const description = document.createElement('p');
+    description.id = 'description';
+    description.textContent = "At Yummy Kitchen, we use fresh, sustainable ingredients to create meals that are both healthy and delicious. Every dish is made with care to nourish your body and satisfy your cravings."
+
+    const exploreMenuBtn = document.createElement('button');
+    exploreMenuBtn.id = 'menu-btn-home';
+    exploreMenuBtn.textContent = 'Explore Menu →';
+
+    textContainer.appendChild(restaurantName)
+    textContainer.appendChild(tagLine);
+    textContainer.appendChild(description);
+    textContainer.appendChild(exploreMenuBtn);
+
+    const imageContainer = document.createElement('div');
+    imageContainer.id = 'img-content';
+
+    const image = document.createElement('img');
+    image.id = 'homepage-img'
+    image.src = homepageImg;
+
+
+    imageContainer.appendChild(image);
+
+    content.appendChild(textContainer);
+    content.appendChild(imageContainer);
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import '@fontsource/inter';
 import './styles/global.css';
 
+import loadHome from './components/homepage';
+
 console.log('hiiiiii')
+
+
+clearContent();
+loadHome();
+
+function clearContent(){
+    const content = document.querySelector('#content');
+    content.innerHTML = '';
+}
