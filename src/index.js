118009101c4cadea2635ab9c0f8acc6ba9933239
madison mirelez <madis123879@gmail.com>
2025-07-22 17:38:22 -0500

    feat: add event listeners to nav buttons

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import '@fontsource/inter';
 import './styles/global.css';
 
 import loadHome from './components/homepage';
+import loadAbout from './components/about.js'
 
-console.log('hiiiiii')
+const homeBtn = document.querySelector('#home-btn')
+const menuBtn = document.querySelector('#menu-btn');
+const abtBtn = document.querySelector('#abt-btn');
+const reserveBtn = document.querySelector('#reserve-btn')
 
+homeBtn.addEventListener('click', () => {
+    clearContent();
+    loadHome();
+})
 
-clearContent();
-loadHome();
+abtBtn.addEventListener('click', () => {
+    clearContent();
+    loadAbout();
+})
+
 
 function clearContent(){
     const content = document.querySelector('#content');
     content.innerHTML = '';
 }
+
+loadHome();
